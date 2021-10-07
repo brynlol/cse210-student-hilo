ca5bbdb7f625896b42446fbca9f26e1c87fb1a45
Josh <rockyjosh14@gmail.com>
2021-10-07 16:38:13 -0400

    I change stuff

--- hilo/game/interface.py
+++ hilo/game/interface.py
@@ -12,7 +12,18 @@ class Interface:
         pass
 
     def check_quit_game(self):
+        quit = input('Keep playing? [y/n] ')
+
+        if quit.lower() == 'y':
+            pass
+        elif score == 0:
+            pass
+        else:
+            pass
+        
         pass
 
     def print_score(self):
+        
+        
         pass
