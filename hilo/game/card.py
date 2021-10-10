0944f0e4306d0e7045b67b74ae12f54a7229ecd4
codermott <91934206+codermott@users.noreply.github.com>
2021-10-09 20:02:00 -0600

    Added comments to the card class.

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -2,14 +2,35 @@ import random
 
 
 class Card:
+    """The responisibility of this class: 
+    --draw the card
+    --validate the user's guess
+    --determine if correct
+    --put a score to the round
 
+    Attributes:
+        --last card: interger of last card drawn
+        --current card: interger of current card drawn
+        --current card type: string that indicates card type
+        --stored guess: sotring the last gues made
+     """
     def __init__(self):
+
+        """The class constructor.
+
+        Declares and initializes instance attributes.
+
+        args: self(Card): instance of Card"""
         self.last_card = 0
         self.current_card = 0
         self.current_card_type = ''
         self.stored_guess = ''
 
     def draw_card(self):
+        """Determines what the card will be.
+
+        args: self(Card): instance of Card
+        """
         self.current_card = random.randint(1, 13)
         card_types = ('spade', 'heart', 'diamond', 'club')
         self.current_card_type = random.choice(card_types)
@@ -18,6 +39,8 @@ class Card:
         """Stores the guess from the interface into an attribute.
         
         Preforms additional validation on input.
+
+        args: self(Card): instance of Card
 
         Returns bool: True on valid input, False on invalid input
         """
@@ -36,6 +59,8 @@ class Card:
     def check_user_guess(self):
         """See if the user's guess is correct.
 
+        args: self(Card): instance of Card
+
         Returns bool: True if correct, False if incorrect
         """
         if self.stored_guess == "h" and self.current_card > self.last_card:
@@ -49,6 +74,11 @@ class Card:
         self.last_card = self.current_card
 
     def mod_score(self):
+        """This determines points for individual rounds.
+        
+        args: self(Card): instance of Card
+
+        returns: Points of the round."""
         if self.check_user_guess():
             points = 100
         elif not self.check_user_guess():
@@ -56,6 +86,11 @@ class Card:
         return points
     
     def pretty_card(self):
+        """Customize look of the card.
+        
+        args: self(Card): instance of Card 
+
+        returns: Picture of the card and description of the card drawn."""
         card_types = {
             'spade': 0x1f0a0,
             'heart': 0x1f0b0,
@@ -80,3 +115,4 @@ class Card:
         pretty_val = card_val[self.current_card]
         unicode_val = card_types[self.current_card_type] + self.current_card
         return f'{chr(unicode_val)} ({pretty_val} of {self.current_card_type.title()}s)'
+
